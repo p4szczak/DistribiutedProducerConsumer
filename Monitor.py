b093a5f489ebd7ceb04c34e50b4452c47c5d42f9
karol <karol.michalowski@student.put.poznan.pl>
2019-05-03 20:58:34 +0200

    Tests on enterCS and exitCS

--- Monitor.py
+++ Monitor.py
@@ -8,62 +8,67 @@ import time
 
 
 comm = MPI.COMM_WORLD
+mutex = threading.Lock()
 
-
-#enum
-class MsgType(Enum):
-    REQUEST = 1
-    TOKEN = 2
+# #enum
+# class MsgType(Enum):
+REQUEST = 1
+TOKEN = 2
 
 class Request:
-    def init(self, ident, seqNo):
+    def __init__(self, ident, seqNo):
         self.ident = ident
         self.seqNo = seqNo
 
 
 class Monitor:
-    def init(self):
+    def __init__(self):
         self.RN = [0] * comm.Get_size()
         self.hasToken = False
         self.inCS = False
         self.ident = comm.Get_rank()
         self.token = None
-        self.mutex = threading.Lock()
-        if(comm.Get_rank() == 0):
+        if(self.ident == 0):
+            print("id = {0} initializing token".format(self.ident))
             self.token = Token([0] * comm.Get_size(), deque([]) , 0, {})
             self.hasToken = True
         t1 = threading.Thread(target= self.recieverThread)
         t1.start()
+        # t1.join()
 
     def enterCS(self):
-        self.mutex.acquire()
-
+        mutex.acquire()
+        print("id = {0} trying to enter to CS".format(self.ident))
         if(not(self.hasToken)):
             self.sendRequest()
             info = MPI.Status()
-            comm.recv(buf = self.token, source = MPI.ANY_SOURCE, tag = MsgType.TOKEN, status = info)
+            print("id = {0} is waiting for token".format(self.ident))
+            self.token = comm.recv(source = MPI.ANY_SOURCE, tag = TOKEN, status = info)
             self.hasToken = True
-
+        print("id = {0} is in CS".format(self.ident))
         self.inCS = True
 
-        self.mutex.release()
+        mutex.release()
 
     def sendRequest(self):
+        self.RN[self.ident] += 1
+        req = Request(self.ident, self.RN[self.ident])
         for i in range(comm.Get_size()):
-            self.RN[self.ident] += 1
-            req = Request(self.ident, self.RN[self.ident])
+            print(type(i))
+            
+            print("id = {0} sending request to {1}".format(self.ident, i))
             if(self.ident == i): continue
-            comm.send(req, dest=i, tag=MsgType.REQUEST)
+            comm.send(obj = req, dest=i, tag=REQUEST)
     
     def exitCS(self):
         # mutex_lock
-        self.mutex.acquire()
-
+        mutex.acquire()
+        print("id = {0} exit CS".format(self.ident))
         self.tokenRelease()
         
         self.inCS = False
         # mutex_unlock
-        self.mutex.release()
+        mutex.release()
 
     def tokenRelease(self):
         for i in range(comm.Get_size()):
@@ -73,24 +78,30 @@ class Monitor:
 
         if self.token.queue:
             newOwner = self.token.queue.popleft()
-            comm.send(obj = self.token, dest = newOwner, tag = MsgType.TOKEN)
+            comm.send(obj = self.token, dest = newOwner, tag = TOKEN)
             self.hasToken = False
             self.token = None
 
     def recieverThread(self):
+        print("id = {0} reciver process started".format(self.ident))
+        print("id = {0} waiting for a message".format(self.ident))
+        
         while True:
             info = MPI.Status()
+            
             comm.probe(source = MPI.ANY_SOURCE, tag = MPI.ANY_TAG, status = info)
             # mutex_lock
-            self.mutex.acquire()
-            if info.MPI.tag == MsgType.REQUEST:
-                req = Request(-1, -1)
-                comm.recv(buf = req, source = MPI.ANY_SOURCE, tag = MsgType.REQUEST)
+            mutex.acquire()
+            if info.tag == REQUEST:
+                
+                req = comm.recv(source = MPI.ANY_SOURCE, tag = REQUEST)
+                print("id = {0} new request from: {1}".format(self.ident, req.ident))
                 self.RN[req.ident] = req.seqNo if self.RN[req.ident] < req.seqNo else self.RN[req.ident]
             
-            if self.hasToken and not(self.inCS):
-                comm.send(obj = self.token, dest = req.ident, tag = MsgType.TOKEN)
-                self.hasToken = False
-                self.token = None
+                if self.hasToken and not(self.inCS):
+                    comm.send(obj = self.token, dest = req.ident, tag = TOKEN)
+                    self.hasToken = False
+                    self.token = None
+
             # mutex_unlock
-            self.mutex.release()
+            mutex.release()
--- main.py
+++ main.py
@@ -2,28 +2,56 @@ from mpi4py import MPI
 from Token import Token
 from Monitor import Monitor
 from collections import deque
-
+import time
+import random
 
 
 if __name__ == '__main__':
 
     comm = MPI.COMM_WORLD
     rank = comm.Get_rank()
+    print(rank)
     mon = Monitor()
-    local = deque([])
-    local.append(1)
-    local.append(10)
-    lista=[1,2,3,4,5,6,7]
-    cond = {"full" : deque([]), "empty" : deque([])}
-    q1 = [1,2,3]
-    cond["full"].append(2)
-    cond["empty"].append(3)
-    cond["asd"] = q1
-    if rank == 0:
-        t1 = Token(lista, local, 2, cond)
-        comm.send(t1, dest=1, tag=11)
-    else:
-        t2 = comm.recv(source=0, tag=11)
-        while t2.queue:
-            print(t2.queue.popleft())
-        print(t2)
+    # if rank == 0:
+    #     pass
+        # local = deque([])
+        # local.append(1)
+        # local.append(10)
+        # lista=[1,2,3,4,5,6,7]
+        # cond = {"full" : deque([]), "empty" : deque([])}
+        # q1 = [1,2,3]
+        # cond["full"].append(2)
+        # cond["empty"].append(3)
+        # cond["asd"] = q1    
+        # t1 = Token(lista, local, 2, cond)
+        # comm.send(t1, dest=1, tag=2)
+        # print(t1)
+    # else:
+    #     mon.enterCS()
+        
+    # while(1):
+    for i in range(2): 
+        # time.sleep(random.randint(0,3))
+    
+        mon.enterCS()
+    # time.sleep(random.randint(1,10))
+        mon.exitCS()
+
+    # local = deque([])
+    # local.append(1)
+    # local.append(10)
+    # lista=[1,2,3,4,5,6,7]
+    # cond = {"full" : deque([]), "empty" : deque([])}
+    # q1 = [1,2,3]
+    # cond["full"].append(2)
+    # cond["empty"].append(3)
+    # cond["asd"] = q1
+    # if rank == 0:
+    #     t1 = Token(lista, local, 2, cond)
+    #     comm.send(t1, dest=1, tag=11)
+    #     print(t1)
+    # else:
+    #     t2 = comm.recv(source=0, tag=11)
+    #     while t2.queue:
+    #         print(t2.queue.popleft())
+    #     print(t2)
